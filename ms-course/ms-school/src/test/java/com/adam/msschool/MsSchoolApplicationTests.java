f7943ed793e2bdef2ccd56d61450e81bf4bc3ffa
Adam Cardoso <adamjuniorcardoso@gmail.com>
2023-10-11 22:37:19 -0300

    Adicionando ms-school ao mono repo

---
+++ ms-course/ms-school/src/test/java/com/adam/msschool/MsSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adam.msschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
